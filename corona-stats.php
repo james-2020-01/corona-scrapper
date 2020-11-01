a5a9bccc7a48720f53ba5c861e8549683f19d524
James Long <james@Jamess-MacBook-Pro.local>
2020-11-01 20:54:58 +0200

    Added comments

--- corona-stats.php
+++ corona-stats.php
@@ -9,7 +9,10 @@ $crawler = $client->request('GET', $url);
 global $countCountries, $countries;
 $countries = Array();
 $countCountries = 0;
+
+//Filter to the table of data
 $crawler->filter('#main_table_countries_today')->each(function ($node) {
+    //Filter only the body of the table and each Row
     $subcrawler = $node->filterXpath("//tbody/tr")->each(function ($subNode) {
         global $count, $country, $countries;
         $attributes = $subNode->extract(['_name', '_text', '_class']);
@@ -19,6 +22,8 @@ $crawler->filter('#main_table_countries_today')->each(function ($node) {
         if($attributes[0][2] == ""){
             $country = Array();
             $count = 0;
+
+            //Filter all cells in the Row
             $subNode->filterXpath("//td")->each(function ($cNode) {
                 global $count, $country;
                 $attr = $cNode->extract(['_name', '_text', '_class']);
@@ -63,9 +68,11 @@ $crawler->filter('#main_table_countries_today')->each(function ($node) {
 $final_countries = array_slice($countries, 8, count($countries)-17);
 
 $json = json_encode($final_countries);
-var_dump($json);
-//
+#var_dump($json);
+// File output for processing later
 $file_output = fopen('coronavirus.json', 'w');
 fwrite($file_output, $json);
-fclose();
+fclose($file_output);
+
+
 ?>
